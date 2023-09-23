41c181f4f1015d7af87e92ccadee4decde93797e
Patrik Šimanský <Gmod4phun@gmail.com>
2023-09-23 19:30:42 +0200

    Keep Atlantis DHD symbol code separated (matches visual layout)

--- code/sbox_stargate/entities/dhd_atlantis/DhdAtlantis.cs
+++ code/sbox_stargate/entities/dhd_atlantis/DhdAtlantis.cs
@@ -24,6 +24,7 @@ public partial class DhdAtlantis : Dhd
 		["C"] = new Vector3( -7.7f, 2.177f, 37.8f ),
 		["D"] = new Vector3( -7.7f, 5.711f, 37.8f ),
 		["E"] = new Vector3( -7.7f, 9.246f, 37.8f ),
+
 		["F"] = new Vector3( -2.488f, -8.425f, 37.8f ),
 		["G"] = new Vector3( -2.488f, -4.893f, 37.8f ),
 		["H"] = new Vector3( -2.488f, -1.358f, 37.8f ),
@@ -31,6 +32,7 @@ public partial class DhdAtlantis : Dhd
 		["J"] = new Vector3( -2.488f, 5.711f, 37.8f ),
 		["K"] = new Vector3( -2.488f, 9.246f, 37.8f ),
 		["L"] = new Vector3( -2.488f, 12.784f, 37.8f ),
+
 		["M"] = new Vector3( 3.467f, -11.959f, 37.8f ),
 		["N"] = new Vector3( 3.467f, -8.425f, 37.8f ),
 		["O"] = new Vector3( 3.467f, -4.893f, 37.8f ),
@@ -40,6 +42,7 @@ public partial class DhdAtlantis : Dhd
 		["R"] = new Vector3( 3.467f, 9.246f, 37.8f ),
 		["S"] = new Vector3( 3.467f, 12.784f, 37.8f ),
 		["T"] = new Vector3( 3.467f, 16.314f, 37.8f ),
+
 		["1"] = new Vector3( 8.403f, -11.959f, 37.8f ),
 		["2"] = new Vector3( 8.403f, -8.425f, 37.8f ),
 		["3"] = new Vector3( 8.403f, -4.893f, 37.8f ),
@@ -49,6 +52,7 @@ public partial class DhdAtlantis : Dhd
 		["7"] = new Vector3( 8.403f, 9.246f, 37.8f ),
 		["8"] = new Vector3( 8.403f, 12.784f, 37.8f ),
 		["9"] = new Vector3( 8.403f, 16.314f, 37.8f ),
+
 		["U"] = new Vector3( 14.292f, -8.425f, 37.8f ),
 		["V"] = new Vector3( 14.292f, -4.893f, 37.8f ),
 		["W"] = new Vector3( 14.292f, -1.358f, 37.8f ),
@@ -56,6 +60,7 @@ public partial class DhdAtlantis : Dhd
 		["X"] = new Vector3( 14.292f, 5.711f, 37.8f ),
 		["Y"] = new Vector3( 14.292f, 9.246f, 37.8f ),
 		["Z"] = new Vector3( 14.292f, 12.784f, 37.8f ),
+
 		["*"] = new Vector3( -9.13f, 20.55f, 37.84f ),
 		["@"] = new Vector3( -4.07f, 20.52f, 37.85f ),
 		["IRIS"] = new Vector3( 9.45f, -22.05f, 35.56f ),
